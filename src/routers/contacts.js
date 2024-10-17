b5ff00c7689589bea713b5cdf390506e1b670a3a
StasAstenenko <107870729+StasAstenenko@users.noreply.github.com>
2024-10-17 21:03:46 +0300

    hw4-validation and sort

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,18 +7,40 @@ import {
   updateContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  contactsValidationScheme,
+  contactsValidationSchemeForPatch,
+} from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
 const jsonParser = express.json();
 
 router.get('/', ctrlWrapper(getContactsController));
 
-router.get('/:contactId', ctrlWrapper(getContactController));
+router.get('/:contactId', isValidId, ctrlWrapper(getContactController));
 
-router.post('/', jsonParser, ctrlWrapper(createContactController));
+router.post(
+  '/',
+  jsonParser,
+  validateBody(contactsValidationScheme),
+  ctrlWrapper(createContactController),
+);
 
-router.patch('/:contactId', jsonParser, ctrlWrapper(updateContactController));
+router.patch(
+  '/:contactId',
+  jsonParser,
+  isValidId,
+  validateBody(contactsValidationSchemeForPatch),
+  ctrlWrapper(updateContactController),
+);
 
-router.delete('/:contactId', jsonParser, ctrlWrapper(deleteContactController));
+router.delete(
+  '/:contactId',
+  jsonParser,
+  isValidId,
+  ctrlWrapper(deleteContactController),
+);
 
 export default router;
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,33 @@
+import { SORT_ORDER } from '../constants/index.js';
 import { Contact } from '../db/models/contactsScheme.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = async () => {
-  const contacts = await Contact.find();
-  return contacts;
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER,
+  sortBy = '_id',
+}) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = Contact.find();
+  const contactsCount = await Contact.find()
+    .merge(contactsQuery)
+    .countDocuments();
+
+  const contacts = await contactsQuery
+    .skip(skip)
+    .limit(limit)
+    .sort({ [sortBy]: sortOrder })
+    .exec();
+
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
 
 export const getContactsById = async (contactId) => {
