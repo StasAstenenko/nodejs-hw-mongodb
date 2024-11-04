8205122e28e6ec7080a08d45062fe2a183d64947
StasAstenenko <107870729+StasAstenenko@users.noreply.github.com>
2024-11-04 16:02:44 +0200

    cloudiary

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -53,7 +53,11 @@ export const createContactController = async (req, res) => {
   let photoUrl;
 
   if (photo) {
-    photoUrl = await saveFileToUploadDir(photo);
+    if (env('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
   }
 
   const contact = {
